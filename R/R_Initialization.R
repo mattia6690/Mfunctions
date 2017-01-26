0edc85c438dbef513af7eb01ea2d6a5d38d80a26
Mattia Rossi <mattia.rossi@eurac.edu>
2017-01-26 18:31:23 +0100

    hello() deleted

--- R/R_Initialization.R
+++ R/R_Initialization.R
@@ -1,14 +1,19 @@
 
 #' @title Loadandinstall
-#' @param mypkg CRAN Package
-#' @description Loads and installs a CRAN hosted PAckage automatically in R.
+#' @description Loads and installs a CRAN hosted Package automatically in R.
 #' No further need to apply both install.package() and library()
-
+#' @param mypkg CRAN Package
 
 loadandinstall <- function(mypkg) {if (!is.element(mypkg, installed.packages()[,1]))
   {install.packages(mypkg)}; library(mypkg, character.only=TRUE)  }
 
 #' @title dircheckup
+#' @description Automatically checks whether a Directory exists. If none is
+#' available it will automatically create one.
+#' By specifying the Subdirectory also non-existing main directories will
+#' be automatically built
+#' @param mypkg Location of a Directory
+
 dircheckup     <- function(mydir) {
   loadandinstall("stringr")
   sp<-str_split(mydir,"/")[[1]]
