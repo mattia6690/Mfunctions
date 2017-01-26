d2c180584c7e49fb285bdd0a11ccf8b10fe65be6
Mattia Rossi <mattia.rossi@eurac.edu>
2017-01-26 18:57:09 +0100

    Extract_2 Function added

--- R/R_Initialization.R
+++ R/R_Initialization.R
@@ -1,8 +1,11 @@
+# R Initialization Functions MR
+
 
 #' @title Loadandinstall
 #' @description Loads and installs a CRAN hosted Package automatically in R.
 #' No further need to apply both install.package() and library()
-#' @param mypkg CRAN Package
+#' @param mypkg character; CRAN Package
+#' @author Christian Zeidler, Universität Würzburg
 
 loadandinstall <- function(mypkg) {if (!is.element(mypkg, installed.packages()[,1]))
   {install.packages(mypkg)}; library(mypkg, character.only=TRUE)  }
@@ -12,7 +15,7 @@ loadandinstall <- function(mypkg) {if (!is.element(mypkg, installed.packages()[,
 #' available it will automatically create one.
 #' By specifying the Subdirectory also non-existing main directories will
 #' be automatically built
-#' @param mypkg Location of a Directory
+#' @param mypkg character; Location of a Directory
 
 dircheckup     <- function(mydir) {
   loadandinstall("stringr")
---
+++ R/R_Spatial.R
@@ -0,0 +1,47 @@
+# R Spatial Functions MR
+
+#' @title Extract 2
+#' @description With this functions raster values are extracted by various
+#' overlaying spatial points created within a polygon shapefile.
+#' @param rr Raster object; raster image
+#' @param pp Spatial object; polygon file
+#' @param points integer; Number of SpatialPoints created in the Polygon
+#' @param sample.type character; Sampling technique for the Spatial Points
+#' generated as described in the spsample() function of the "sp" package
+#' @param weight boolean; Weighting image considering the underlaying rasters a
+#' differentweight in the extraction computation. This makes pixel partially
+#' located within a polygon less weighted. Input is T/F
+#' @param sd boolean; computes the standard deviation of each extracted raster
+#' @param narm boolean; removes na values
+#' @param seed integer; applies the set.seed() function enabling the control over
+#' @usage extract2<- function (rr,pp,points=1000,samp.type="regular",weight=F,sd=F,narm=F,seed=1)
+
+extract2<- function (rr,pp,points=1000,samp.type="regular",weight=F,sd=F,narm=F,seed=1){
+
+  #Transform the Raster to Array (much faster and cellnumbers)
+  t5<-as.array(values(rr))
+  #Initialize Array for the Values and the Cells used
+  val<-array(dim=nrow(pp))
+  ncells<-array(dim=nrow(pp))
+  if (sd==T){stdev<- array(dim=nrow(pp))}
+  for (fi in 1:nrow(pp)){
+    set.seed(seed)
+    # Create a regular sampled Points in every Polygon
+    u<-spsample(pp[fi,],n=points,type=samp.type)
+    # Obtain the unique Cellnumbers intersecting with the Points
+    if (weight==T){t1<- cellFromXY(rr,u)}
+    if (weight==F){t1<- unique(cellFromXY(rr,u))}
+    # Compute the Mean of the Cellvalues
+    t2<- mean(t5[t1],na.rm=narm)
+    if (sd==T){t3<- sd(t5[t1],na.rm=narm)}
+    # Write the Values to the Arrays
+    val[fi]<-t2
+    ncells[fi]<- length(unique(t1))
+    if (sd==T){stdev[fi]<- t3}
+    if (fi%%5000==0){print(str_c(fi, " of ", nrow(pp)))}
+  }
+  # Combine both Arrays in one Dataframe and return the result
+  if(sd==F){stat<-data.frame(val,ncells)}
+  if(sd==T){stat<-data.frame(val,ncells,stdev)}
+  return(stat)
+}
